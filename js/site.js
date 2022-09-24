56d7c650ae78a4eee6df94e8fae3c53ce34283b5
Rating001 <106230549+Rating001@users.noreply.github.com>
2022-09-23 23:26:36 -0700

    Coded the base of TacoCat.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,46 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+function getInput() {
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById('userString').value;
+    let cleanedString = cleanString(userString);
+    let reversedString = reverseString(cleanedString);
+    compareAndPrint(userString,cleanedString,reversedString);
+
+}
+
+function cleanString(userString) {
+    let cleanedString = '';
+
+    cleanedString = userString.replace(/[^a-zA-Z]/g, '');
+    cleanedString = cleanedString.toLowerCase();
+    return cleanedString;
+}
+
+function reverseString(cleanedString) {
+    let reversedString = '';
+
+    for (let i = ((cleanedString.length) - 1); i >= 0; i--) {
+        reversedString += cleanedString[i];
+    }
+
+    return reversedString;
+}
+
+function compareAndPrint(original,cleaned,reversed){
+
+    let alertHeader = document.getElementById('alertHead').innerHTML;
+    let alertBox = document.getElementById('msg').innerHTML;
+    alertBox = '';
+    alertHeader = '';
+
+    if (cleaned == reversed) {
+        document.getElementById('alertHead').innerHTML = 'Congratulations! \"' + original + '\" is a palindrome!';
+        document.getElementById('msg').innerHTML = 'You\'ve mastered time and space! Time to kick back and enjoy life!'
+    } else {
+        document.getElementById('alertHead').innerHTML = 'Sorry! \"' + original + '\" is not a palindrome.';
+        document.getElementById('msg').innerHTML = 'Remember that a palindrom must mirror itself front to back and back to front.'
+    }
+
+    document.getElementById("alert").classList.remove("invisible");
+
 }
